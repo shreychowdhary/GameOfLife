3c811598fbc109f622af5695516001cb44e4958a
Shrey <shrey.chowdhary1@gmail.com>
2016-09-16 09:54:26 -0400

    test load files

--- js/life.js
+++ js/life.js
@@ -31,7 +31,7 @@ function initateGrid(){
 	}
 	$.ajax({url:"https://api.github.com/repos/shreychowdhary/GameOfLife/contents/txt/start.txt",
 		success:function(data){
-			console.log(typeof(data.contents));
+			console.log(data.contents);
 			for(i = gridheight/2-25; i < gridheight/2+25; i++){
 				for(j = gridwidth/2-100; j < gridwidth/2+100; j++){
 					grid[j][i] = data.content.charAt((i-(gridheight/2-25))*201+(j-(gridwidth/2-100)));
